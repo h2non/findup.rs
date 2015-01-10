47c8442dc5cbca8058214550e16f5371181f1b33
Tomas Aparicio <tomas@aparicio.me>
2015-01-11 00:04:02 +0100

    feat(test)

--- tests/findup.rs
+++ tests/findup.rs
@@ -2,10 +2,12 @@
 
 extern crate findup;
 
-use findup::findup;
 use std::os;
 use std::io;
 use std::io::TempDir;
+use std::io::fs:: PathExtensions;
+
+use findup::findup;
 
 macro_rules! assert_eq { 
   ($e1:expr, $e2:expr) => (
@@ -26,5 +28,11 @@ fn mk_file(path: &str, directory: bool) {
 #[test]
 fn find_file_in_current_directory() {
   let file = findup("Cargo.toml");
-  assert_eq!(file.exists(), true)
+  assert_eq!(file.exists(), true);
+}
+
+#[test]
+fn find_glob_pattern_in_current_directory() {
+  let file = findup("*.toml");
+  assert_eq!(file.exists(), true);
 }
