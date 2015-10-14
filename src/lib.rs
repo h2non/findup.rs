2c3bb37def3f4bc9659c605341a39a018b3739e0
Tomás Aparicio <tomas@aparicio.me>
2015-10-14 13:06:00 +0100

    refactor: remove warn attribute

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![doc(html_logo_url = "http://www.rust-lang.org/logos/rust-logo-128x128-blk-v2.png",
        html_favicon_url = "http://www.rust-lang.org/favicon.ico")]
 #![allow(unstable)]
-#[warm(unstable)]
 
 extern crate glob;
 
